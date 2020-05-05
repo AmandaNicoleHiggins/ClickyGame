166956c35260c35e12ce3e5fbc81a9b7478dcb6c
Amanda Higgins <amanda.n.higgins@gmail.com>
2020-05-04 20:03:22 -0400

    Updates

---
+++ precache-manifest.cd955c572e1485fb5bf74f677774db87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1ad0c676391faaf09717ae52857cfd0",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "abbb40dc9228b8a3a3a1",
+    "url": "/clicky-game/static/css/main.e6b823a6.chunk.css"
+  },
+  {
+    "revision": "5feb00ff3c1967f0e545",
+    "url": "/clicky-game/static/js/2.d6612644.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.d6612644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abbb40dc9228b8a3a3a1",
+    "url": "/clicky-game/static/js/main.904305c3.chunk.js"
+  },
+  {
+    "revision": "b631d7080dcb249ab8e5",
+    "url": "/clicky-game/static/js/runtime-main.ea51dac7.js"
+  }
+]);
